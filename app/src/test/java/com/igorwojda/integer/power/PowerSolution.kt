e835cbe79090526eaca4ddf4f7254edacf7b95c1
Sanket <hellosanketk@gmail.com>
2019-02-08 14:02:10 +0530

    Clean up and restore log

--- app/src/test/java/com/igorwojda/integer/power/PowerSolution.kt
+++ app/src/test/java/com/igorwojda/integer/power/PowerSolution.kt
@@ -3,6 +3,7 @@ package com.igorwojda.integer.power
 // Recursive solution
 private object Solution1 {
     private fun power(base: Int, exponent: Int): Int {
+        println("$base, $exponent")
         if (exponent == 1) {
             return base
         }
@@ -13,7 +14,6 @@ private object Solution1 {
 private object Solution2 {
     private fun power(base: Int, exponent: Int): Int {
         println("$base, $exponent")
-
         return when(exponent) {
             in Int.MIN_VALUE..-1 -> {
                 val b = 1 / base
