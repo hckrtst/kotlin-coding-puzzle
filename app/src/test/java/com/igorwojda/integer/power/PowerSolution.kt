6fdb4d2ad46425cfedf7b34dadefc6958a11d053
Sanket <hellosanketk@gmail.com>
2019-02-08 13:46:33 +0530

    Update solution and tests for Power

--- app/src/test/java/com/igorwojda/integer/power/PowerSolution.kt
+++ app/src/test/java/com/igorwojda/integer/power/PowerSolution.kt
@@ -4,11 +4,17 @@ package com.igorwojda.integer.power
 private object Solution {
     private fun power(base: Int, exponent: Int): Int {
         println("$base, $exponent")
-        if (exponent == 1) {
-            return base
-        }
 
-        return base * power(base, exponent - 1)
+        return when(exponent) {
+            in Int.MIN_VALUE..-1 -> {
+                val b = 1 / base
+                val e = -exponent
+                b * power(b, e - 1)
+            }
+            0 -> 1
+            1 -> base
+            else -> base * power(base, exponent - 1)
+        }
     }
 }
 
